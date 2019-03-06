6f47489b5a6f49621be41cf6d15457b69989f62c
kunark <kunark.khewal@tothenew.com>
2019-03-06 23:20:57 +0530

    sentiment analysis done by js

--- main.js
+++ main.js
@@ -7,21 +7,36 @@ function preload(){
 
 function setup(){
     noCanvas();
-    // console.log(afinn);
+    console.log(afinn);
 
     var txt = select('#txt');
     txt.input(typing);
-    // txt = txt.value;
-    // console.log(" type of txt before",txt);
-
-    // txt = String(txt);
-
-
-    // console.log(" type of txt after",txt);
 
     function typing(){
-        var words = txt.split(/\W/);
+        var textinput = txt.value();
+        var words = textinput.split(/\W/);
         console.log(words);
+        var scoredwords = [];
+        var totalscore = 0;
+        var score = 0;
+        for (var i = 0; i < words.length; i++){
+            var word = words[i].toLowerCase();
+            if(afinn.hasOwnProperty(word)){
+                var score = afinn[word];
+                console.log(word, afinn[word]);
+                totalscore += Number(score);
+                scoredwords.push(word + ": " + score + " ");
+            }
+        }
+
+        var scoreP = select('#score');
+        scoreP.html('score: ' + totalscore);
+
+        var comp = select('#comparative');
+        comp.html('comparative: ' + totalscore/words.length);
+
+        var wordlist = select('#wordlist');
+        wordlist.html(scoredwords);
         // console.log(txt.value());
     }
 }
